71070f64ca24f80c1a71f1b8e29aa05f235e6383
francis leon <francisleon1186@yahoo.com>
2025-02-26 09:48:40 -0500

    se creo un panel de jsx and css

---
+++ src/PanelControl.jsx
@@ -0,0 +1,42 @@
+//function PanelControl(){}
+import './PanelControl.css';
+const PanelControl =() =>{
+    return (
+        <section>
+            <aside>
+                <div>
+                    <img src="" alt ="Logo"/>
+                </div>
+                <nav>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </nav>
+            </aside>
+            <section>
+                <div>
+                    <section></section>
+                    <section>
+                        <p>Ver</p>
+                    </section>
+                </div>
+                <div>
+                    <section></section>
+                    <section>
+                        <p>Ver</p>
+                    </section>
+                </div>
+                <div>
+                    <section></section>
+                    <section>
+                        <p>Ver</p>
+                    </section>
+                </div>
+            </section>
+        </section>
+    )
+    }
+export default PanelControl
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import PanelControl from './PanelControl'
+//esto aqui arriba sale por defecto una vez escrito abajo en el strict mode 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   
+    <PanelControl/>
   </StrictMode>,
 )
